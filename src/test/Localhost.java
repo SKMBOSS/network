0d121d3f82bc024d7a032d21d6feb8b28bb375f7
SKMBOSS <zzangjungsu@gmail.com>
2019-04-26 17:25:24 +0900

    이름바꾸기

--- src/test/Localhost.java
+++ src/test/Localhost.java
@@ -1,41 +1,72 @@
 package test;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Localhost {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		try {
+			InetAddress inetAddress = 
+					InetAddress.getLocalHost();
 			
-			InetAddress inetAddress = InetAddress.getLocalHost();
-			
-			String hostName = inetAddress.getHostName();
+			String hostname = inetAddress.getHostName();
 			String hostAddress = inetAddress.getHostAddress();
-			
-			System.out.println(hostName + " : " + hostAddress);
+			System.out.println( hostname + ":" + hostAddress);
 			
 			byte[] addresses = inetAddress.getAddress();
-			
-			for(byte address : addresses) {
-				System.out.print(address & 0x000000ff);
+			for( byte address : addresses ) {
+				System.out.print( address & 0x000000ff );
+				System.out.print( '.' );
 			}
-			
-			for(int i=0; i< addresses.length; i++) {
-				
+			System.out.print("\n-------------------------------\n");
+
+			for( String IPAddress : hostIPAddresses() ) {
+				System.out.println(IPAddress);
 			}
-			
-//			InetAddress[] inetAddresses =InetAddress.getAllByName(hostName);
-//			
-//			for(InetAddress addr : inetAddresses) {
-//				System.out.println(addr.getHostAddress());
-//			}
 			
 		} catch (UnknownHostException e) {
-			
+			e.printStackTrace();
+		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public static List<String> hostIPAddresses() throws SocketException {
+		List<String> result = new ArrayList<String>();
+
+		for (NetworkInterface ifc : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+
+			// 활성화 된 NIC이 아니면 무시
+			if (ifc.isUp() == false) {
+				continue;
+			}
+
+			// Loopback(127.0.0.1)이면 무시
+			if (ifc.isLoopback() == true) {
+				continue;
+			}
+
+			//if (ifc.isVirtual() == true) {
+			//	continue;
+			//}
+			
+			for (InetAddress inetAddr : Collections.list(ifc.getInetAddresses())) {
+				
+				// Link Address(MAC Adress 무시)
+				if (inetAddr.isLinkLocalAddress() == true) {
+					continue;
+				}
+				
+				result.add(inetAddr.getHostAddress());
+			}
+		}
+		
+		return result;
+	}
 }
