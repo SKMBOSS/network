63e0da4adbfdbb5397b03f46b534b8937355251f
SKMBOSS <zzangjungsu@gmail.com>
2019-04-26 16:59:37 +0900

    convert Scanner with BufferReader

--- src/util/NSLookup.java
+++ src/util/NSLookup.java
@@ -1,41 +1,50 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Scanner;
+
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-	
-		Scanner sc = null;
+
+		BufferedReader br = null;
+
 		try {
-			sc = new Scanner(System.in);
-			
-			while(true) {
-				System.out.print(">");
-				String hostName = sc.nextLine();
-				
-				if(hostName.equals("exit")) break;
-				
+			br = new BufferedReader(new InputStreamReader(System.in, "utf-8"));
+
+			String hostName = null;
+			System.out.print(">");
+
+			while ((hostName = br.readLine()) != null) {
+
+				if (hostName.equals("exit"))
+					break;
+
 				try {
 					InetAddress[] inetAddresses = InetAddress.getAllByName(hostName);
-					
-					for(int i=0; i<inetAddresses.length; i++) {
+
+					for (int i = 0; i < inetAddresses.length; i++) {
 						System.out.println(inetAddresses[i]);
-						
 					}
-					
+
 				} catch (UnknownHostException e) {
-					//e.printStackTrace();
-					System.out.println("Àß¸øµÈ Çü½ÄÀÔ´Ï´Ù.");
-				}	
+					// e.printStackTrace();
+					System.out.println("ìž˜ëª»ëœ ì ‘ê·¼ìž…ë‹ˆë‹¤.");
+				}
 			}
 		} catch (Exception e) {
-			
+
 		} finally {
-			if (sc != null) {
-				sc.close();
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
